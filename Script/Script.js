cb574f4083f5ac14204ed31a9fca344fb4374e47
luanvoss <luanmateusxj@gmail.com>
2026-09-15 20:47:34 -0300

    Em andamento

--- Script/Script.js
+++ Script/Script.js
@@ -1,5 +1,3 @@
-
-
 const Pokedex = document.querySelector(".Pokedex");
 const gridPokemon = document.getElementById("gridPokemon");
 const modalBody = document.getElementById("modalBody");
@@ -7,11 +5,11 @@ const modalTitle = document.getElementById("modalTitle");
 const pokemonModalElement = document.getElementById("pokemonModal");
 const pokemonModal = new bootstrap.Modal(pokemonModalElement);
 let todosPokemons = [];
-Pokedex.style.display = "none";
+
 
 async function PegarPokemon() {
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
         const data = await response.json();
         todosPokemons = await Promise.all(
             data.results.map(async (pokemon) => {
@@ -27,11 +25,11 @@ async function PegarPokemon() {
 }
 
 function renderizarPokemons() {
-    console.log("todosPokemons:", todosPokemons);
     gridPokemon.innerHTML = "";
     todosPokemons.forEach(pokemon => {
+        const tipo = pokemon.types[0].type.name;
         const card = document.createElement("div");
-        card.classList.add("card", "col-3");
+        card.classList.add("card", "col-3", tipo);
         card.style.width = "18rem";
         card.innerHTML = `
             <img 
@@ -49,7 +47,7 @@ function renderizarPokemons() {
 }
 PegarPokemon();
 function AbrirModal(pokemon) {
-    modalTitle.textContent = pokemon.name
+    modalTitle.textContent = pokemon.name;
     modalBody.innerHTML = `
             <img 
                 src="${pokemon.sprites.other["official-artwork"].front_default}" 
@@ -61,33 +59,46 @@ function AbrirModal(pokemon) {
             </div>
                 <div class="attack">
         <h5 class="card-title">
-        Ataque: ${pokemon.stats.find(stat => stat.stat.name === "attack").base_stat}
-    </h5>
-</div>
-<div class="defense">
-    <h5 class="card-title">
-        Defesa: ${pokemon.stats.find(stat => stat.stat.name === "defense").base_stat}
-    </h5>
-</div>
-<div class="hp">
-    <h5 class="card-title">
-        HP: ${pokemon.stats.find(stat => stat.stat.name === "hp").base_stat}
-    </h5>
+            <i class="bi bi-lightning-charge-fill"></i>
+            ${pokemon.stats.find(stat => stat.stat.name === "attack").base_stat}
+        </h5>
     </div>
+
+    <div class="defense">
+        <h5 class="card-title">
+            <i class="bi bi-shield-fill"></i>
+            ${pokemon.stats.find(stat => stat.stat.name === "defense").base_stat}
+        </h5>
+    </div>
+
+    <div class="hp">
+        <h5 class="card-title">
+            <i class="bi bi-heart-fill"></i>
+            ${pokemon.stats.find(stat => stat.stat.name === "hp").base_stat}
+        </h5>
+    </div>
+
     <div class="speed">
-    <h5 class="card-title">
-        Velocidade: ${pokemon.stats.find(stat => stat.stat.name === "speed").base_stat}
-    </h5>
-</div>
-<div class="weight">
-    <h5 class="card-title">Peso: ${pokemon.weight / 10} kg</h5>
-</div>
+        <h5 class="card-title">
+            <i class="bi bi-wind"></i>
+            ${pokemon.stats.find(stat => stat.stat.name === "speed").base_stat}
+        </h5>
+    </div>
 
-<div class="height">
-    <h5 class="card-title">Altura: ${pokemon.height / 10} m</h5>
-</div>
+    <div class="weight">
+        <h5 class="card-title">
+            <i class="bi bi-speedometer2"></i>
+            ${pokemon.weight / 10} kg
+        </h5>
+    </div>
 
-        `;
+    <div class="height">
+        <h5 class="card-title">
+            <i class="bi bi-arrows-vertical"></i>
+            ${pokemon.height / 10} m
+        </h5>
+    </div>
+`;
     pokemonModal.show();
 }
 
