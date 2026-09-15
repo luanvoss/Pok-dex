05e248edb2b17a9add54760bbbffd12a9b8ac2ab
luanvoss <luanmateusxj@gmail.com>
2026-09-14 21:44:57 -0300

    Em andamento

--- Script/Script.js
+++ Script/Script.js
@@ -1,29 +1,26 @@
 
-const pokemon = document.getElementById("Pokemon");
-const Imagem = document.getElementById("Imagem");
-const Nome = document.getElementById("Nome");
-const Altura = document.getElementById("Altura");
-const Peso = document.getElementById("Peso");
-const Tipo = document.getElementById("Tipo");
-const Ataque = document.getElementById("Ataque");
-const Defesa = document.getElementById("Defesa");
-const HP = document.getElementById("HP");
-const Velocidade = document.getElementById("Velocidade");
+
 const Pokedex = document.querySelector(".Pokedex");
 const gridPokemon = document.getElementById("gridPokemon");
-
+const modalBody = document.getElementById("modalBody");
+const modalTitle = document.getElementById("modalTitle");
+const pokemonModalElement = document.getElementById("pokemonModal");
+const pokemonModal = new bootstrap.Modal(pokemonModalElement);
+let todosPokemons = [];
 Pokedex.style.display = "none";
 
 async function PegarPokemon() {
     try {
         const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
         const data = await response.json();
-        TodosPokemons = await Promise.all(
+
+        todosPokemons = await Promise.all(
             data.results.map(async (pokemon) => {
                 const response = await fetch(pokemon.url);
                 return await response.json();
             })
         );
+
         renderizarPokemons();
     } catch (error) {
         console.log(error);
@@ -31,60 +28,68 @@ async function PegarPokemon() {
 }
 
 function renderizarPokemons() {
-    console.log("todosPokemons:", TodosPokemons);
-    TodosPokemons.forEach(pokemon => {
-        gridPokemon.innerHTML += `
-            <div class="card col-3" style="width: 18rem;">
-                <img 
-                    src="${pokemon.sprites.other["official-artwork"].front_default}" 
-                    class="card-img-top" 
-                    alt="${pokemon.name}"
-                >
-                <div class="card-body">
-                    <h5 class="card-title">${pokemon.name}</h5>
-                </div>
+    console.log("todosPokemons:", todosPokemons);
+    gridPokemon.innerHTML = "";
+    todosPokemons.forEach(pokemon => {
+        const card = document.createElement("div");
+        card.classList.add("card", "col-3");
+        card.style.width = "18rem";
+        card.innerHTML = `
+            <img 
+                src="${pokemon.sprites.other["official-artwork"].front_default}" 
+                class="card-img-top" 
+                alt="${pokemon.name}"
+            >
+            <div class="card-body">
+                <h5 class="card-title">${pokemon.name}</h5>
             </div>
         `;
-    });
-    const cards = document.querySelectorAll(".card");
-    cards.forEach((card, indice) => {
-        card.addEventListener("click", () => {
-            const poke = TodosPokemons[indice];
-            Pokedex.style.display = "block";
-            Imagem.src = poke.sprites.front_default;
-            Nome.value = poke.name;
-            Altura.value = poke.height;
-            Peso.value = poke.weight;
-            Tipo.value = poke.types[0].type.name;
-            Ataque.value = poke.stats[1].base_stat;
-            Defesa.value = poke.stats[2].base_stat;
-            HP.value = poke.stats[0].base_stat;
-            Velocidade.value = poke.stats[5].base_stat;
-            Pokedex.scrollIntoView({
-                behavior: "smooth"
-            });
-        });
+        card.addEventListener("click", () => AbrirModal(pokemon));
+        gridPokemon.appendChild(card);
     });
 }
 PegarPokemon();
+function AbrirModal(pokemon) {
+    modalTitle.textContent = pokemon.name
+    modalBody.innerHTML = `
+            <img 
+                src="${pokemon.sprites.other["official-artwork"].front_default}" 
+                class="card-img-top" 
+                alt="${pokemon.name}"
+            >
+            <div class="card-body">
+                <h5 class="card-title">${pokemon.name}</h5>
+            </div>
+                <div class="attack">
+        <h5 class="card-title">
+        Ataque: ${pokemon.stats.find(stat => stat.stat.name === "attack").base_stat}
+    </h5>
+</div>
+<div class="defense">
+    <h5 class="card-title">
+        Defesa: ${pokemon.stats.find(stat => stat.stat.name === "defense").base_stat}
+    </h5>
+</div>
+<div class="hp">
+    <h5 class="card-title">
+        HP: ${pokemon.stats.find(stat => stat.stat.name === "hp").base_stat}
+    </h5>
+    </div>
+    <div class="speed">
+    <h5 class="card-title">
+        Velocidade: ${pokemon.stats.find(stat => stat.stat.name === "speed").base_stat}
+    </h5>
+</div>
+<div class="weight">
+    <h5 class="card-title">Peso: ${pokemon.weight / 10} kg</h5>
+</div>
 
-pokemon.addEventListener("blur", async () => {
-    try {
-        const response = await fetch(
-            `https://pokeapi.co/api/v2/pokemon/${pokemon.value}`
-        );
-        const poke = await response.json();
-        Pokedex.style.display = "block";
-        Imagem.src = poke.sprites.front_default;
-        Nome.value = poke.name;
-        Altura.value = poke.height;
-        Peso.value = poke.weight;
-        Tipo.value = poke.types[0].type.name;
-        Ataque.value = poke.stats[1].base_stat;
-        Defesa.value = poke.stats[2].base_stat;
-        HP.value = poke.stats[0].base_stat;
-        Velocidade.value = poke.stats[5].base_stat;
-    } catch (error) {
-        console.log("Pokémon não encontrado");
-    }
-});
+<div class="height">
+    <h5 class="card-title">Altura: ${pokemon.height / 10} m</h5>
+</div>
+
+        `;
+    pokemonModal.show();
+}
+
+
